ab7dfa85093622c366fcc8c256280568d3131e5f
TheVixhal <142595758+TheVixhal@users.noreply.github.com>
2024-04-25 22:47:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import sqlite3
 from crewai import Crew
 from agents import LegalAgents
 from tasks import LegalTasks
